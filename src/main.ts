29e3eeb3dd5da59a86cc5da93390331e46d112be
Matthew Roche <matthew@matthewroche.co.uk>
2024-01-06 22:12:21 +0000

    Changed title, removed favicon, removed unnecessary files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import VueApexCharts from "vue3-apexcharts";
 
 import App from './App.vue'
@@ -9,7 +8,6 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(VueApexCharts);
 
